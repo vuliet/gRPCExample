c0c082b18e19cafd9c514a5bb23865fc587ef208
ThaiVu <thai35.tv@gmail.com>
2023-11-24 14:52:23 +0700

    Add project files.

---
+++ gRPCClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using gRPCClient.Protos;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7230");
+var client = new UserProtoService.UserProtoServiceClient(channel);
+
+var request = new GetUserRequest
+{
+    UserId = "1"
+};
+
+try
+{
+    var user = client.GetUser(request);
+
+    Console.WriteLine(user);
+}
+catch (RpcException ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ gRPCExample/Procedures/UserService.cs
@@ -0,0 +1,14 @@
+﻿using Grpc.Core;
+using gRPCServer.Protos;
+using static gRPCServer.Protos.UserProtoService;
+
+namespace gRPCServer.Procedures
+{
+    public class UserService : UserProtoServiceBase
+    {
+        public override Task<UserResponse> GetUser(GetUserRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new UserResponse { UserId = request.UserId, UserName = "ThaiVu" });
+        }
+    }
+}
